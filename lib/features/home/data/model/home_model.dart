13e24491cb34c31d9246e14548217614384f780c
mahmoud <eslamimahmoud98@gmail.com>
2023-02-03 12:04:30 +0330

    feat: update Home model

--- lib/features/home/data/model/home_model.dart
+++ lib/features/home/data/model/home_model.dart
@@ -1,7 +1,8 @@
 class HomeModel {
   List<Projects>? projects;
+  List<Skills>? skills;
 
-  HomeModel({this.projects});
+  HomeModel({this.projects, this.skills});
 
   HomeModel.fromJson(Map<String, dynamic> json) {
     if (json["projects"] is List) {
@@ -11,14 +12,79 @@ class HomeModel {
               .map((e) => Projects.fromJson(e))
               .toList();
     }
+    if (json["skills"] is List) {
+      skills = json["skills"] == null
+          ? null
+          : (json["skills"] as List).map((e) => Skills.fromJson(e)).toList();
+    }
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
+    final Map<String, dynamic> _data = <String, dynamic>{};
     if (projects != null) {
-      data["projects"] = projects?.map((e) => e.toJson()).toList();
+      _data["projects"] = projects?.map((e) => e.toJson()).toList();
     }
-    return data;
+    if (skills != null) {
+      _data["skills"] = skills?.map((e) => e.toJson()).toList();
+    }
+    return _data;
+  }
+}
+
+class Skills {
+  String? title;
+  String? description;
+  List<SkillItems>? skillItems;
+
+  Skills({this.title, this.description, this.skillItems});
+
+  Skills.fromJson(Map<String, dynamic> json) {
+    if (json["title"] is String) {
+      title = json["title"];
+    }
+    if (json["description"] is String) {
+      description = json["description"];
+    }
+    if (json["skillItems"] is List) {
+      skillItems = json["skillItems"] == null
+          ? null
+          : (json["skillItems"] as List)
+              .map((e) => SkillItems.fromJson(e))
+              .toList();
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> _data = <String, dynamic>{};
+    _data["title"] = title;
+    _data["description"] = description;
+    if (skillItems != null) {
+      _data["skillItems"] = skillItems?.map((e) => e.toJson()).toList();
+    }
+    return _data;
+  }
+}
+
+class SkillItems {
+  String? title;
+  String? image;
+
+  SkillItems({this.title, this.image});
+
+  SkillItems.fromJson(Map<String, dynamic> json) {
+    if (json["title"] is String) {
+      title = json["title"];
+    }
+    if (json["image"] is String) {
+      image = json["image"];
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> _data = <String, dynamic>{};
+    _data["title"] = title;
+    _data["image"] = image;
+    return _data;
   }
 }
 
@@ -46,13 +112,13 @@ class Projects {
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data["title"] = title;
-    data["description"] = description;
-    data["banner"] = banner;
+    final Map<String, dynamic> _data = <String, dynamic>{};
+    _data["title"] = title;
+    _data["description"] = description;
+    _data["banner"] = banner;
     if (images != null) {
-      data["images"] = images;
+      _data["images"] = images;
     }
-    return data;
+    return _data;
   }
 }
