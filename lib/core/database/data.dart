35832d991486676bc03cfdf1a9c78cc0d2c17af3
mahmoud <eslamimahmoud98@gmail.com>
2023-02-04 14:56:59 +0330

    feat: add project images to data

--- lib/core/database/data.dart
+++ lib/core/database/data.dart
@@ -46,21 +46,48 @@ var dataset = {
       "description":
           "CodeHub is a Persian pastebin service that provides all kind of software services completely Free and Open Source.",
       "banner": "",
-      "images": []
+      "images": [
+        "https://raw.githubusercontent.com/codehub-ir/codehub-mobile/main/screen-shots/13.png",
+        "https://raw.githubusercontent.com/codehub-ir/codehub-mobile/main/screen-shots/14.png",
+        "https://raw.githubusercontent.com/codehub-ir/codehub-mobile/main/screen-shots/15.png",
+        "https://raw.githubusercontent.com/codehub-ir/codehub-mobile/main/screen-shots/16.png",
+        "https://raw.githubusercontent.com/codehub-ir/codehub-mobile/main/screen-shots/17.png",
+        "https://raw.githubusercontent.com/codehub-ir/codehub-mobile/main/screen-shots/18.png",
+        "https://raw.githubusercontent.com/codehub-ir/codehub-mobile/main/screen-shots/19.png",
+      ]
     },
     {
       "title": "Barber booking",
       "description":
           "A barber shop booking platform using Firebase as the backend service and Flutter as the client technology.",
       "banner": "",
-      "images": []
+      "images": [
+        "https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/1.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/2.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/3.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/4.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/5.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/6.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/7.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/8.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/9.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/10.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/11.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/12.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/barber_booking/master/screen_shots/13.png",
+      ]
     },
     {
       "title": "Meal planners",
       "description":
           "A desktop app that helps users find recipes with detailed information.",
       "banner": "",
-      "images": []
+      "images": [
+        "https://raw.githubusercontent.com/mahmoud-eslami/resume/main/images/meal%20planner/meal-planner-1.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/resume/main/images/meal%20planner/meal-planner-2.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/resume/main/images/meal%20planner/meal-planner-3.png",
+        "https://raw.githubusercontent.com/mahmoud-eslami/resume/main/images/meal%20planner/meal-planner-4.png",
+      ]
     }
   ],
   "skills": [
