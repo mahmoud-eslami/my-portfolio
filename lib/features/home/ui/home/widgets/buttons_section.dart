1b56c4ab78104e7d44cf24a27a05e71f6101864b
mahmoud <eslamimahmoud98@gmail.com>
2023-03-14 12:54:52 +0330

    feat: get resume link from repository

--- lib/features/home/ui/home/widgets/buttons_section.dart
+++ lib/features/home/ui/home/widgets/buttons_section.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_portfolio/core/constants/constants.dart';
 import 'package:flutter_portfolio/core/global_widgets/animation_builder.dart';
 import 'package:flutter_portfolio/core/utils/url_launcher.dart';
 
 import '../../../../../core/global_widgets/custom_button.dart';
 import '../../../../../core/utils/size_checker.dart';
+import '../../../bloc/bloc/home_bloc.dart';
 
 class ButtonsSection extends StatelessWidget {
   const ButtonsSection({super.key});
@@ -21,13 +23,29 @@ class ButtonsSection extends StatelessWidget {
       return AnimationBuilder(
         child: Column(
           children: [
-            CustomButton(
-              title: "Download CV",
-              onPressed: () {
-                launchTheUrl(Constants.resumeUrl);
+            BlocBuilder<HomeBloc, HomeState>(
+              builder: (context, state) {
+                String link = "https://google.com";
+                if (state is FetchHomeDataSuccess) {
+                  link = state.homeData.resume!;
+                }
+                if (state is FetchHomeDataLoading) return Container();
+
+                return CustomButton(
+                  title: (state is FetchHomeDataFailure)
+                      ? "Error: tap to try again!"
+                      : "Download CV",
+                  onPressed: () {
+                    if (state is FetchHomeDataSuccess) {
+                      launchTheUrl(link);
+                    } else if (state is FetchHomeDataFailure) {
+                      context.read<HomeBloc>().add(HomeDataFetched());
+                    }
+                  },
+                  width: size.width * .4,
+                  height: Constants.mobileBtnHeight,
+                );
               },
-              width: size.width * .4,
-              height: Constants.mobileBtnHeight,
             ),
             CustomButton(
               title: "Hire me!",
